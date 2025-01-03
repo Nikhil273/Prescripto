e3d147250419de9e2ac54431db747985c2dc29b8
Nikhil273 <nikhilmaurya.it@gmail.com>
2025-01-03 20:10:39 +0530

    Creating Admin Panel

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,7 +1,13 @@
+// filepath: /C:/Users/Asus/Desktop/p/frontend/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5174,
+    strictPort: true, // Fail if the port is already in use
+    open: true, // Open the browser automatically
+    host: '0.0.0.0', // Allow access from network
+  }
 })
